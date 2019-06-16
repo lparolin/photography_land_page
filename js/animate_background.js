126ceba71e85b229d9e0838fd91ed6d5c328ab28
Luca Parolini <lucapar@gmail.com>
2019-06-16 21:45:14 +0200

    Almost working for splash page

--- js/animate_background.js
+++ js/animate_background.js
@@ -1,7 +1,9 @@
 const active_class = "active";
 const next_class = "next";
 const inactive_class = "inactive";
-const fadeout_duration = 100;
+const class_array = [active_class, next_class, inactive_class];
+const all_classes = class_array.join(" ");
+const fadeout_duration = 2000;
 const update_image_interval = 5000;
 
 function initialize_images($all_images) {
@@ -12,26 +14,24 @@ function initialize_images($all_images) {
     set_next($all_images.eq(next_image_index));
 }
 
-function remove_animation_classes($image) {
-    $image.removeClass(active_class);
-    $image.removeClass(next_class);
-    $image.removeClass(inactive_class);
+function remove_classes($image, classes=all_classes) {
+    $image.removeClass(classes);
 }
 
 function set_active($image) {
-    remove_animation_classes($image);
+    remove_classes($image, class_array.join(" "));
     $image.addClass(active_class);
 }
 
 function set_next($image) {
-    remove_animation_classes($image);
+    remove_classes($image);
     $image.addClass(next_class);
 }
 
 function set_inactive($image) {
-    remove_animation_classes($image);
+    remove_classes($image);
     $image.addClass(inactive_class);
-    $image.css("display", "block");
+    //$image.css("display", "block");
 }
 
 function get_following_image_index($image_set, image_index) {
@@ -59,9 +59,15 @@ function animate_images() {
     var active_image_index = 0;
     window.setInterval(function() {
         $active_image = $image_set.eq(active_image_index);
+
+        // fadeout sets display to none at the end of the anymation
+        // this is not what we want. Therefore at the end of the anymation
+        // we remove the display setting, getting what the parent item has
         $active_image.fadeOut(fadeout_duration, function() {
+            $active_image.css("display", "flex");
             active_image_index = update_classes($image_set, active_image_index);
         });
+
     }, update_image_interval);
 }
 
