6994032bd282ec565fb7679fb8954fc02372ade7
Luca Parolini <lucapar@gmail.com>
2019-07-01 23:06:01 +0200

    Minor changes

--- minify.sh
+++ minify.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Missing version number. Usage: $0 VERSION_NUMBER" > &2
+    exit 1
+fi
+
 rm js/land_page.min.js
 ~/Downloads/minify_2.5.0_darwin_amd64/minify -o js/land_page.min.js js
 
